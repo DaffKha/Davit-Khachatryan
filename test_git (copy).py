01f82d0aad1f73a99d84d84294e767004288dbe4
dkhachatryan-mdsol <dkhachatryan@mdsol.com>
2020-06-08 01:32:54 +0400

    hasgd

---
+++ test_git (copy).py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+df = pd.read_csv('pix.csv')
+print(df.head())
+
+df1 = pd.read_csv('pix.csv')
+print(df1.head())
+
+assert df = df1
+assert df = df1
+assert df = df1
+assert df = df1
